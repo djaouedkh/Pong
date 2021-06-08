3cd6f49525ab36296a2d4e18ae073e15fd727dea
Djaoued <khatir.djaoued@gmail.com>
2021-06-08 21:33:49 +0200

    commentary translation

--- pong.js
+++ pong.js
@@ -3,59 +3,58 @@ var game;
 var playerHeight = 100;
 var playerWidth = 5;
 
-// CREATION DU TERRAIN-----------------------------------------------------------
+// CREATION OF THE FIELD-----------------------------------------------------------
 function terrain(){
     
-    // .GETCONTEXT('2D') SERT A CREER UN CONTEXT 2D DONT LEQUEL ON PEUT DESSINER
+    // .GETCONTEXT('2D') CREATE A 2D CONTEXT WHERE YOU CAN DRAW
     var context = canvas.getContext('2d');
 
 //---------------------
 
-    // Dessin du terrain
+    // DRAWING OF THE FIELD
 
-    // SERT A DONNER UNE COULEUR AU CONTEXT 2D
+    // COLOR IN CONTEXT 2D
     context.fillStyle = 'black';
-    // (x POINT DE DEPART ABSCISSE, y PT DE DEPART ORDONNEE, LARGEUR, HAUTEUR) DU CONTEXT 2D
+    // (x starting point horizontal, y starting point vertical, width, height) CONTEXT 2D
     context.fillRect(0, 0, canvas.width, canvas.height);
     
 
  //-------------------   
 
-    // Dessin de la ligne médiane
+    // DRAWING OF THE MEDIAN LINE
 
-    // DONNE UNE COULEUR A LA LIGNE
+    // COLOR LINE
     context.strokeStyle = 'white';
-    // CREER UNE LIGNE
+    // CREATION OF THE LINE
     context.beginPath();
-    // GERE LE PT DE DEPART DE LA LIGNE (x,y)
-    // dans notre cas pr que la ligne se trouve tjr au centre on prend la largeur de notre canvas qu'on divise par 2
+    // Line starting point (x,y)
+    // in our case so that the line is always in the center we take the width of our canvas that we divide by 2
     context.moveTo(canvas.width / 2, 0);
-    // GERE LE PT DE DEPART DE LA LIGNE (x,y)
-    // cest les coordonnées du pt d'arrivée de la ligne
+    // Line ending point (x,y)
     context.lineTo(canvas.width / 2, canvas.height);
-    // AFFICHE LA LIGNE
+    // DISPLAYS THE LINE
     context.stroke();
 
 
 
 //---------------------
     
-    // Dessin des players
+    // DRAWING PLAYERS
 
 
     context.fillStyle = 'white';
     
     // PLAYER LEFT
-    // (x,y, LARGEUR, HAUTEUR)
+    // (x,y, width, height)
     context.fillRect(0, game.player.y, playerWidth, playerHeight);
 
     //PLAYER RIGHT
-    // dans ce cas le x est defini par largeur du terrain - largeur player pour qu il soit a l'interieur du terrain 
+    // in this case the x is defined by the width of the field - width player so that it is inside the field 
     context.fillRect(canvas.width - playerWidth, game.computer.y, playerWidth, playerHeight);
 
 //---------------------
 
-    // Dessin balle
+    // DRAWING BALL
 
     context.beginPath();
     context.fillStyle = 'white';
@@ -67,12 +66,12 @@ function terrain(){
     
 }
 
-// LANCEMENT DE TOUS LE JEU-----------------------------------------------------
+// GAME LAUNCH-----------------------------------------------------
 document.addEventListener('DOMContentLoaded', function () {
     play();
 });
 
-// OBJET GAME CONTENANT PLAYER COMPUTER BALL-------------------------------------
+// GAME OBJECT CONTAINING PLAYER COMPUTER BALL-------------------------------------
     game = {
         player: {
             y: canvas.height / 2 - playerHeight / 2
@@ -96,23 +95,23 @@ document.addEventListener('DOMContentLoaded', function () {
 function changeDirection(playerPosition) {
     var impact = game.ball.y - playerPosition - playerHeight / 2;
     var ratio = 100 / (playerHeight / 2);
-    // VALEUR ENTRE 0 ET 10
+    // VALUE BETWEEN 0 TO 10
     game.ball.speed.y = Math.round(impact * ratio / 10);
 }
 
 function collision(player){
-    // SI PLAYER RATE LA BALLE
+    // SI PLAYER MISSES THE BALL
     if (game.ball.y < player.y || game.ball.y > player.y + playerHeight) {
-        // PLACE BALL ET PLAYER AU CENTRE
+        // PLACE THE BALL AND PLAYER IN THE CENTER
         game.ball.x = canvas.width / 2;
         game.ball.y = canvas.height / 2;
         game.player.y = canvas.height / 2 - playerHeight / 2;
         game.computer.y = canvas.height / 2 - playerHeight / 2;
         
-        // RESET LA VITESSE
+        // RESET SPEED
         game.ball.speed.x = 2;
     } 
-    // SINON AUGMENTE LA VITESSE DE LA BALL ET CHANGE LA DIRECTION
+    // ELSE INCREASES THE SPEED OF THE BALL AND CHANGES THE DIRECTION
     else {       
         game.ball.speed.x *= -1.2;
         changeDirection(player.y);
@@ -122,7 +121,7 @@ function collision(player){
 
 function ballMove(){
     
-    // BALL REBONDIT SUR LE HAUT ET BAS DU TERRAIN
+    // BALL BOUNCES ON THE TOP AND BOTTOM OF THE COURT
     if (game.ball.y > canvas.height || game.ball.y < 0) {
         game.ball.speed.y *= -1;
     }
@@ -135,7 +134,7 @@ function ballMove(){
     }
 
 
-    // ON AJOUTE A BALL.X LA VALEUR DE BALL.SPEED.X ET PAREIL POUR Y
+    // WE ADD TO BALL.X THE VALUE OF BALL.SPEED.X AND THE SAME FOR Y
     game.ball.x += game.ball.speed.x;
     game.ball.y += game.ball.speed.y;
 }
@@ -147,7 +146,7 @@ function play(){
     ballMove();
     computerMove();
 
-    // PERMET DE RELANCER PLUSIEURS FOIS LA FONCTION PLAY POUR AVOIR L IMPRESSION QUE LA BALLE TRANSLATE
+    // RESTART THE PLAY FUNCTION SEVERAL TIMES TO GET THE IMPRESSION THAT THE BALL IS TRANSLATING
     requestAnimationFrame(play);
 }
 
@@ -156,17 +155,17 @@ function play(){
 
 // MOUSE EVENT PLAYER
 canvas.addEventListener('mousemove', playerMove);
-// EVENT EST UN CALLBACK, DANS CE CAS IL S AGIT DU CANVAS
+// EVENT IS A CALLBACK, IN THIS CASE IT IS THE CANVAS
 function  playerMove(event) {
 
-    // .GETBOUNDINGCLIENTRECT() PERMET DE RECUPERER TOUTES LES VALEURS D UN OBJET (x,y,width,height,top,right...)
+    // .GETBOUNDINGCLIENTRECT()  RETRIEVE ALL VALUES OF AN OBJECT (x,y,width,height,top,right...)
     var canvasLocation = canvas.getBoundingClientRect();
 
-    // EVENT.CLIENTY = LA POSITION Y DE LA SOURIS DANS LE CANVAS CAR EVENT => CANVAS
-    //ON SOUSTRAIT A EVENTCLIENTY LA VALEUR Y DU CANVAS QU ON RECUPERER AVEC GetBoundingClientRect()
+    // EVENT.CLIENTY = THE Y POSITION OF THE MOUSE IN THE CANVAS , EVENT => CANVAS
+    // WE SUBTRACT FROM EVENTCLIENTY THE Y VALUE OF THE CANVAS WHICH WE RECOVER WITH GetBoundingClientRect()
     var mouseLocation = event.clientY - canvasLocation.y;
 
-    // ON MODIFIE LA POSITION Y DU PLAYER AU MVMT DE LA SOURIS ET ON CENTRE
+    // WE CHANGE THE POSITION Y OF THE PLAYER WITH THE MOUSE AND WE CENTER
     game.player.y = mouseLocation - playerHeight / 2;
     
     if (mouseLocation < playerHeight / 2) {
@@ -181,37 +180,10 @@ function  playerMove(event) {
 
 }
 
-// -----------------------------------METHODE PLAYERMOVE DJAOUED------------------------------------
-
-    //DJAOUED PLAYER MOVE
-// canvasDimension = canvas.getBoundingClientRect();
-
-// canvas.addEventListener('mousemove', function playerMove(e){
-    // PERMET DE CENTRER LA SOURIS PAR RAPPORT AU PLAYER
-    // game.player.y = (e.y - canvasDimension.y) - playerHeight / 2;
-
-    // BLOQUE LA SOURIS EN HAUT
-    // if (game.player.y <= 0) {
-    //     game.player.y = 0;
-    // }
-    // BLOQUE LA SOURIS EN BAS
-//     else if (game.player.y >= (canvas.height - playerHeight)) {
-//         game.player.y = (canvas.height - playerHeight)
-//     }
-// })
-// ---------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
 // MOUSE EVENT COMPUTER
 
 function computerMove(){
-    // ON MODIFIE LA POSITION Y DU COMPUTER ON LUI AJOUTONS LA POSITION LA POSITION Y DE LA BALLE 
-    // ON LE MULTIPLIE PAR 0.85 POUR QU IL AI UN PEU DE RETARD ET GAGNE
+    // WE MODIFY THE Y POSITION OF THE COMPUTER WE ADD THE Y POSITION OF THE BALL 
+    // WE MULTIPLY IT BY 0.85 TO GIVE DELAY TO THE COMPUTER
     game.computer.y += game.ball.speed.y * 0.85;
 }
